53812e7b63d7f15faa454192b00c476bc9f5e4bc
Rohit Cursor <purecoder7@gmail.com>
2026-01-21 23:47:28 +0530

    added multer for cloudinary

---
+++ backend/middlewares/cloudinaryUpload.js
@@ -0,0 +1,20 @@
+import multer from "multer"
+import cloudinary from "../config/cloudinary"
+import { CloudinaryStorage } from "multer-storage-cloudinary"
+
+const storage = new CloudinaryStorage({
+    cloudinary, 
+    params: async (req, file) => {
+        const fileType = file.mimetype.startsWith("video") ? "video": "image";
+
+        return {
+            folder: "social_web",
+            resource_type: fileType,
+            public_id: Date.now().toString()
+        }
+    }
+})
+
+
+const uploadCloudinary = multer({storage})
+export default uploadCloudinary
